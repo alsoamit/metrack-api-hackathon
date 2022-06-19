8d14ed85e5a2c509f907a457047af3e9b5b1b848
Amit Kumar <alsoamit@gmail.com>
2022-06-19 14:46:49 +0530

    ADD mail service

---
+++ src/services/mail-service.js
@@ -0,0 +1,29 @@
+import { createTransport } from "nodemailer";
+
+class MailService {
+  async send(email, subject, body) {
+    try {
+      const transporter = createTransport({
+        host: process.env.HOST,
+        service: process.env.SERVICE,
+        port: 587,
+        secure: true,
+        auth: {
+          user: process.env.USER,
+          pass: process.env.PASS,
+        },
+      });
+
+      return await transporter.sendMail({
+        from: process.env.USER,
+        to: email,
+        subject: subject,
+        html: body,
+      });
+    } catch (error) {
+      return null;
+    }
+  }
+}
+
+export default new MailService();
