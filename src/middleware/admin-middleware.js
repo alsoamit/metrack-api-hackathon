c9e5e4b49bdfd5224ef4d5be3995256e48161f8c
Amit Kumar <alsoamit@gmail.com>
2022-06-20 12:38:13 +0530

    ADD admin middleware

---
+++ src/middleware/admin-middleware.js
@@ -0,0 +1,19 @@
+import APIResponse from "../helpers/APIResponse";
+
+async function verifyAdmin(req, res, next) {
+  try {
+    const { role } = req.user;
+    if (role === undefined) {
+      return APIResponse.unauthorizedResponse(res, "user not found");
+    }
+    if (role >= 2) {
+      return next();
+    }
+    return APIResponse.unauthorizedResponse(res, "invalid admin");
+  } catch (err) {
+    console.log({ err });
+    return APIResponse.unauthorizedResponse(res);
+  }
+}
+
+export default verifyAdmin;
